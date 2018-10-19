ef32f3f226a1c1055c0be6b18b75037840e47473
tom_glb <goodMorning_glb@atguigu.com>
2018-10-19 19:55:41 +0800

    ②在加入了后台管理员实体类时，引入了自定义注解和权限类 ①研究了注解的简单原理 ③查看谁调用过这两个接口，然后发现其中有一个反射的工具类，所以复习了一下反射的基本操作 ④在反射的工具类中有一个Object转为Map的方法，其中调用了Apache的BeanUtils工具类，将其转化 ⑤通过这样的类比，FastJson中的操作可以更方便的转换Map操作

---
+++ bbs_common/src/main/java/com/jk/eop/resource/model/AuthAction.java
@@ -0,0 +1,75 @@
+package com.jk.eop.resource.model;
+
+import com.jk.framework.database.DynamicField;
+import com.jk.framework.database.PrimaryKeyField;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * 权限点实体
+ * @author xiaoxiaoqiang
+ * 2018-10-19
+ * @version 1.1
+ */
+public class AuthAction extends DynamicField implements Serializable{
+
+	private static final long serialVersionUID = -4857392266664309246L;
+	/**
+	 * 权限点Id
+	 */
+	private Integer actid;
+	
+	/**
+	 * 权限的名称
+	 */
+	private String name;
+	
+	/**
+	 * 权限类型
+	 */
+	private String type;
+	
+	/**
+	 * 对象值
+	 */
+	private String objvalue;	
+	
+	/**
+	 * 是否为系统默认权限
+	 */
+	private int choose;
+	
+	@PrimaryKeyField
+	public Integer getActid() {
+		return actid;
+	}
+	public void setActid(Integer actid) {
+		this.actid = actid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getObjvalue() {
+		return objvalue;
+	}
+	public void setObjvalue(String objvalue) {
+		this.objvalue = objvalue;
+	}
+	public int getChoose() {
+		return choose;
+	}
+	public void setChoose(int choose) {
+		this.choose = choose;
+	}
+}
