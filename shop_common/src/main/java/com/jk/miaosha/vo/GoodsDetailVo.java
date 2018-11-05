adbadd7890cb8fff0ecb23c412f7342ec1d88b9b
tom_glb <goodMorning_glb@atguigu.com>
2018-11-05 23:25:29 +0800

    静态化页面加入的视图VO

---
+++ shop_common/src/main/java/com/jk/miaosha/vo/GoodsDetailVo.java
@@ -0,0 +1,22 @@
+package com.jk.miaosha.vo;
+
+import com.jk.miaosha.domain.MiaoshaUser;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName : GoodsDetailVo
+ * @Author : xiaoqiang
+ * @Date : 2018/11/5 21:54:54
+ * @Description :
+ * @Version ： 1.0
+ */
+@Data
+public class GoodsDetailVo implements Serializable {
+    private static final long serialVersionUID = 3959469170335222800L;
+    int miaoshaStatus = 0; //秒杀状态 0表示还没开始 1 进行中 2结束
+    int remainSeconds = 0; //秒杀剩余时间 0进行中 -1已结束
+    private MiaoshaUser user;
+    private GoodsVo goodsVo;
+}
